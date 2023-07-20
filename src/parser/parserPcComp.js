05fadef618550d49f28777b5e6ff01a7de256f94
DPachoGit <davidpacho1513@gmail.com>
2023-07-20 17:45:01 +0200

    Parser creado

---
+++ src/parser/parserPcComp.js
@@ -0,0 +1,49 @@
+import {JSDOM} from 'jsdom';
+
+class Parser {
+    constructor(html) {
+        this.html = html;
+        this.dom = new JSDOM(this.html);
+    }
+    getCards = () => {
+        return this.dom.window.document.querySelectorAll("#product-grid > div");
+    }
+    getTitle = (card) => {
+        return card.querySelector(".product-card__title").textContent.trim();
+    }
+    getPrice = (card) => {
+        return card.querySelector(".sc-kOtXPA").textContent.trim();
+    }
+    getImage = (card) => {
+        return card.querySelector(".sc-gdlCkj").getAttribute("src");
+    }
+    getUrl = (card) => {
+        return "https://amazon.es"+card.querySelector("a").getAttribute("href");
+    }
+    getCard = (card) => {
+        return {
+            title: this.getTitle(card),
+            price: this.getPrice(card),
+            image: this.getImage(card),
+            url: this.getUrl(card)
+        };
+    }
+    
+    getCardsArray = () => {
+        const cards = this.getCards();
+        const cardsArray = [];
+        for(let card of cards){
+            try{
+                cardsArray.push(this.getCard(card));
+            }
+            catch(e){
+                console.log(e);
+            }
+        }
+        return cardsArray;
+    }
+
+
+}
+
+export default Parser;
